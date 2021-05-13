8dbd0cd453adf3d182991c1c69faaeb8c9167552
mtugrulavci <mustafa.tugrulavci@alcumus.com>
2021-05-13 00:59:08 -0400

    questions validation and timer is set

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,27 @@
 var count = 30;
-var i = 0;
+var questionCounter = 0;
 var a = 1;
 var counter = document.querySelector('#counter');
 var startBtn = document.querySelector('.startBtn');
 var start = document.querySelector('.start');
-
+var container = document.querySelector('.container');
 var questionContainer = document.querySelector('#question-container');
 var questionElement = document.querySelector('.question');
 var choice1 = document.querySelector('.choice-1');
 var choice2 = document.querySelector('.choice-2');
 var choice3 = document.querySelector('.choice-3');
 var choice4 = document.querySelector('.choice-4');
+var result = document.querySelector('.result');
 
 var answerBtn = document.querySelectorAll('.answer-btn');
-
-var selectedButton = document.querySelectorAll('.choices');
 var answer = document.querySelector(".selected-answer");
+
 
 const questions  = [
   {
       question: "Commonly used data type Do Not include:---",
       choices: ["strings","booleance","alerts", "numbers"],
-      answer : "alerts"    
+      answer : "alerts"   
   },
   {
       question: "The condition in an if/else statement is enclosed within:---",
@@ -31,17 +31,17 @@ const questions  = [
   {
       question: "Arrays in JavaScript can be used to store:---",
       choices: ["numbers and strings","others Arrays","booleances", "all of the above"],
-      answer : "all of the above"    
+      answer : "all of the above"   
   },
   {
       question: "String values must be enclosed within --- when being assigned to variables ",
       choices: ["commas","curly brackets","quotes","parentheses"],
-      answer : "quotes"    
+      answer : "quotes" 
   },
   {
       question: "A very useful tool used during development and debugging for printing content to the debugger is:---",
       choices: ["JavaScript","terminal/bash","alerts", "console.log"],
-      answer : "console.log"    
+      answer : "console.log"   
   },
 ]
 function startQuiz(){
@@ -52,20 +52,55 @@ function startQuiz(){
     }, 1000);
     start.removeChild(startBtn);
 
-    getQuestions();
+    getQuestions(questionCounter);
     questionContainer.className = ".choice";
 
 };
+answerBtn[0].addEventListener("click", checkAnswer);
+answerBtn[1].addEventListener("click", checkAnswer);
+answerBtn[2].addEventListener("click", checkAnswer);
+answerBtn[3].addEventListener("click", checkAnswer);
 
-function getQuestions(){
-  questionElement.textContent = 'Question  ' + a + " : " + questions[i].question;
-  choice1.textContent = questions[i].choices[0];
-  choice2.textContent = questions[i].choices[1]; 
-  choice3.textContent = questions[i].choices[2];
-  choice4.textContent = questions[i].choices[3]; 
-  i++;
-  a++;
+
+
+function getQuestions(questionCounter){
+    questionElement.textContent = 'Question  ' + a + " : " + questions[questionCounter].question;
+    choice1.textContent = questions[questionCounter].choices[0];
+    choice2.textContent = questions[questionCounter].choices[1];
+    choice3.textContent = questions[questionCounter].choices[2];
+    choice4.textContent = questions[questionCounter].choices[3];
+    a++;
 };
+function checkAnswer(e){
+    if(e.target.textContent === questions[questionCounter].answer){
+        answer.textContent = "Correct!"
 
-startBtn.addEventListener("click", startQuiz);
-answerBtn.forEach((btn)=>{ btn.addEventListener("click",getQuestions); });
+    } else{
+        answer.textContent = "Wrong!"
+        if(count>5){
+            count = count - 5;
+        }else{
+            count = 0;
+        }
+    }
+    if (questionCounter+1< questions.length){
+    questionCounter++;
+    getQuestions(questionCounter);
+    } else {
+        gameOver();
+
+    }
+};
+function gameOver(){
+    questionContainer.classList.add("hide");
+    result.innerHTML = "Your Score is :" +count;
+
+};
+startBtn.addEventListener("click", startQuiz);    
+
+
+
+
+
+
+  
